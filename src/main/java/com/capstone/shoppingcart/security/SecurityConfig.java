47c3a9dbbdc0cff690438a7271961ea94c6047d1
SOKNA CHUN <chun.sokna.1222@gmail.com>
2025-12-18 13:05:42 +0700

    update authentication endpoint path to include customer version

--- src/main/java/com/capstone/shoppingcart/security/SecurityConfig.java
+++ src/main/java/com/capstone/shoppingcart/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/customer/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
