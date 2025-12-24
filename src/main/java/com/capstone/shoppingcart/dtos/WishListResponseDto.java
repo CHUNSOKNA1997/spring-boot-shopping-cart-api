e7d3b25511880fb89049e27257a10dceb5448fc2
SOKNA CHUN <chun.sokna.1222@gmail.com>
2025-12-24 12:47:50 +0700

    add WishListResponseDto for managing wishlist data, including ID, user ID, product list, and creation timestamp

---
+++ src/main/java/com/capstone/shoppingcart/dtos/WishListResponseDto.java
@@ -0,0 +1,21 @@
+package com.capstone.shoppingcart.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WishListResponseDto {
+    private UUID id;
+    private UUID userId;
+    private List<ProductDto> products;
+    private LocalDateTime createdAt;
+}
